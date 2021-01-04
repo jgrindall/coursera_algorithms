8a5b6baacec5a13bf541ac9c1f8c51b5c5e6db39
jgrindall <john.grindall@gmail.com>
2021-01-04 08:51:27 +0000

    1. fix reading file's last line.
    2. add Hamming distance (question 2)

--- course3/quiz2/app.ts
+++ course3/quiz2/app.ts
@@ -2,8 +2,8 @@
 import * as _ from 'lodash';
 import fs from 'fs';
 import {AdjList} from './AdjList';
-import {Kruskal, getTotalWeight} from './Kruskal';
 import {Cluster, Clustering} from './Clustering';
+import {UnionFind} from "./UnionFind";
 
 fs.readFile('./quiz2/cluster1.txt', 'utf8', (err:any, data:string) => {
     if (err){
@@ -14,24 +14,62 @@ fs.readFile('./quiz2/cluster1.txt', 'utf8', (err:any, data:string) => {
     lines.forEach((line:string, i:number)=>{
         if(i >= 1){
             const parts:Array<string> = line.split(' ');
-            //console.log(parts);
-            g.addNode(parts[0]);
-            g.addNode(parts[1]);
+            if(parts.length >= 3){
+                g.addNode(parts[0]);
+                g.addNode(parts[1]);
+            }
         }
     });
     lines.forEach((line:string, i:number)=>{
         if(i >= 1){
             const parts:Array<string> = line.split(' ').map(s => s.trim());
-            g.addEdge(parts[0], parts[1], parseInt(parts[2]));
-            g.addEdge(parts[1], parts[0], parseInt(parts[2]));
+            if(parts.length >= 3){
+                g.addEdge(parts[0], parts[1], parseInt(parts[2]));
+                g.addEdge(parts[1], parts[0], parseInt(parts[2]));
+            }
         }
     });
     const clustering:Clustering = new Clustering(g);
     const clusters:Array<Cluster> = clustering.getClusters(4);
     console.log('clusters', clusters.length, clustering.getSpacing());
     console.log(clusters);
+});
 
-    // find the spacing
+const flip = (s:string, i:number):string=>{
+    const a = s.split('');
+    a[i] = (a[i] === "0" ? "1" : "0");
+    return a.join('');
+};
 
+fs.readFile('./quiz2/cluster2.txt', 'utf8', (err:any, data:string) => {
+    const NUM_DIGITS:number = 24;
+    if (err){
+        throw err;
+    }
+    const lines = data.split('\n').map(s=>s.trim().replace(/ /g, ''));
+    const deDup = Array.from(new Set(lines));
+    const unionFind = new UnionFind(deDup);
+    console.log("single flip");
+    deDup.forEach(node=>{
+        _.range(0, NUM_DIGITS).forEach(index=>{
+            const s = flip(node, index);
+            if(unionFind.contains(s)){
+                unionFind.union(node, s);
+            }
+        });
+    });
+    console.log("double flip");
+    deDup.forEach(node=>{
+        _.range(0, NUM_DIGITS - 1).forEach(index0=>{
+            _.range(index0, NUM_DIGITS).forEach(index1=>{
+                const s = flip(flip(node, index0), index1);
+                if(unionFind.contains(s)){
+                    unionFind.union(node, s);
+                }
+            });
+        });
+    });
+
+    console.log(unionFind.getNumComponents());
 
 });
